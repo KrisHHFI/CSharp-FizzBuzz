ad7508cb2d87fcab555726c3e80b57ec8b6e3d07
KrisHHFI <kristopherpepperuk@gmail.com>
2024-10-28 10:32:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+A simple C# "Fizz Buzz" program
+*/
+Console.WriteLine("Fizz Buzz - - - - -");
+
+for (int i = 1; i <= 100; i++)
+{
+    // If a multiple of 3 and 5 "Fizz Buzz"
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    // If divisible by 3 "Fizz"
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    // If a multiple of 5 "Buzz"
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
